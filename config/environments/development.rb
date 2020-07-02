977fce4da7fbcb367bc879a855509905ae19206d
Ubuntu <ubuntu@ip-172-31-35-161.eu-central-1.compute.internal>
2020-07-02 00:13:36 +0000

    Kullanıcı Giriş/Üye Ol

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
+  config.action_mailer.default_url_options = { host: 'https://c7cedc76bebe434593b48ecaedc6a75f.vfs.cloud9.eu-central-1.amazonaws.com/' }
+  
 end
